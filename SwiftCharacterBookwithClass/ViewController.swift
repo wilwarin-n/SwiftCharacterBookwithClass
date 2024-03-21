42b09563e36a5f2974f2d28cfde2468989993632
Rumeysa <michiru.aware.lrn@gmail.com>
2024-03-22 01:12:49 +0300

    Added prepare didselect functions

--- SwiftCharacterBookwithClass/ViewController.swift
+++ SwiftCharacterBookwithClass/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var tableView: UITableView!
     
     var characterList = [Character]()
+    var userSelect : Character?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +25,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         let ganyu = Character(characterName: "Ganyu", characterElement: "Cryo", characterArea: "Liyue", image: UIImage(named: "ganyu")!)
         
-        let hutao = Character(characterName: "Hu Tao", characterElement: "Pyro", characterArea: "Liyue", image: UIImage(named: "hutao")!)
+        let hutao = Character(characterName: "Hu Tao", characterElement: "Pyro", characterArea: "Liyue", image: UIImage(named: "hu tao")!)
         
         let nahida = Character(characterName: "Nahida", characterElement: "Dendro", characterArea: "Sumeru", image: UIImage(named: "nahida")!)
         
@@ -42,8 +43,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        userSelect = characterList[indexPath.row]
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC"{
+            let destinationVC = segue.destination as! DetailsViewController
+            destinationVC.selectedCharacter = userSelect
+        }
     }
 
 
