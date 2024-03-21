233653505e30cc902c45db50de07ebd9e80aef6c
Rumeysa <michiru.aware.lrn@gmail.com>
2024-03-21 23:45:17 +0300

    Create class for detailsviewcontroller

---
+++ SwiftCharacterBookwithClass/DetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  SwiftCharacterBookwithClass
+//
+//  Created by Aydın Kutlu on 21.03.2024.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
