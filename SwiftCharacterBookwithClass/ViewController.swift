1d635dcc4a50cbdb46d6f73283a5862c86c63a67
Rumeysa <michiru.aware.lrn@gmail.com>
2024-03-22 00:17:38 +0300

    Added objects for characters info

--- SwiftCharacterBookwithClass/ViewController.swift
+++ SwiftCharacterBookwithClass/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        //Characters
+        let traveler = Character(characterName: "Traveler", characterElement: "Anemo", characterArea: "Mondstat", image: UIImage(named: "traveler")!)
+        
+        let ganyu = Character(characterName: "Ganyu", characterElement: "Cryo", characterArea: "Liyue", image: UIImage(named: "ganyu")!)
+        
+        let hutao = Character(characterName: "Hu Tao", characterElement: "Pyro", characterArea: "Liyue", image: UIImage(named: "hutao")!)
+        
+        let nahida = Character(characterName: "Nahida", characterElement: "Dendro", characterArea: "Sumeru", image: UIImage(named: "nahida")!)
+        
+        let characterList = [traveler,ganyu,hutao,nahida]
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -22,6 +38,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
+        cell.textLabel?.text = "Test"
         return cell
     }
 
