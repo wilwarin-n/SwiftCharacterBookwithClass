e3d39883c7df001798a6ad150cc6fb3a704fe90f
Rumeysa <michiru.aware.lrn@gmail.com>
2024-03-21 23:25:56 +0300

    Added rows and cells info

--- SwiftCharacterBookwithClass/ViewController.swift
+++ SwiftCharacterBookwithClass/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
     }
 
 
