cd5c97be04da973e0fb06cd7b555d67e5f654274
Rumeysa <michiru.aware.lrn@gmail.com>
2024-03-22 01:02:03 +0300

    Connect labels and image view
    Sınıfla bağladık viewcontroller kısmını

--- SwiftCharacterBookwithClass/DetailsViewController.swift
+++ SwiftCharacterBookwithClass/DetailsViewController.swift
@@ -9,21 +9,22 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    @IBOutlet weak var arealabel: UILabel!
+    @IBOutlet weak var elementlabel: UILabel!
+    @IBOutlet weak var namelabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedCharacter : Character?
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        namelabel.text = selectedCharacter?.characterName
+        elementlabel.text = selectedCharacter?.characterElement
+        arealabel.text = selectedCharacter?.characterArea
+        imageView.image = selectedCharacter?.image
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
