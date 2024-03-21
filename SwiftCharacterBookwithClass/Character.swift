ee6bfad64975e04460e77999ef068efe3a6984b8
Rumeysa <michiru.aware.lrn@gmail.com>
2024-03-22 00:07:14 +0300

    Added image object in the class

--- SwiftCharacterBookwithClass/Character.swift
+++ SwiftCharacterBookwithClass/Character.swift
@@ -6,15 +6,18 @@
 //
 
 import Foundation
+import UIKit
 
 class Character{
     var characterName : String
     var characterElement : String
     var characterArea : String
+    var image : UIImage
     
-    init(characterName : String, characterElement : String, characterArea: String){
+    init(characterName : String, characterElement : String, characterArea: String, image : UIImage){
         self.characterName = characterName
         self.characterElement = characterElement
         self.characterArea = characterArea
+        self.image = image
     }
 }
